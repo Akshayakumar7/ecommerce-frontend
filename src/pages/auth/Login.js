8200132a884694e5f2c24c6e8c97db36c8ce9139
Akshay Kumar <akshaykumar@veenamtech.com>
2024-02-24 09:25:52 +0530

    testing

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -43,6 +43,8 @@ const Login = () => {
         setLoading(false);
     };
 
+    console.log("Hello")
+
     const onClickForgotPassword = async () => {
         navigate("/forgot-password")
     }
